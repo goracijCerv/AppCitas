76237d7f7a340a38b66e7963105b06ce2ec144ed
goracijCerv <100542978+goracijCerv@users.noreply.github.com>
2022-09-04 16:08:02 -0500

    cambios del viernes 2/09
    Se agregaron data y entities al proyecto al igual que se descargo framework de entityes

---
+++ Apcitas.WebService/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Apcitas.WebService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Apcitas.WebService.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<AppUser> Users { get; set; }
+}
---
+++ Apcitas.WebService/Entities/AppUser.cs
@@ -0,0 +1,26 @@
+﻿namespace Apcitas.WebService.Entities;
+
+public class AppUser
+{
+    //la forma de c# se puede resumir en esta linea
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    
+
+    //ESta es la froma en  la que se realiza en c#
+    /*private int myVar;
+    public int MyVar
+    {
+        get { return myVar; }
+        set { myVar = value; }
+    }*/
+    //equivalente a lo anterior realizado pero en focado a lo que conocemos comun mente
+    /*
+     * public int getMyVar(){
+     *    return MyVar;
+     * }
+     * public void setMyVar(int valor){
+     *    MyVar=valor;
+     * }
+      */
+}
