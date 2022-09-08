59a118282d29c72081d54a1c4576b59d18bf2b1b
goracijCerv <100542978+goracijCerv@users.noreply.github.com>
2022-09-08 07:58:11 -0500

    se creao el controlador de usuario y se hizo asíncrono
    cambios del 7 y 8 de septiembre

---
+++ Apcitas.WebService/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Apcitas.WebService.Data;
+using Apcitas.WebService.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Apcitas.WebService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly DataContext _context;
+    public UserController(DataContext context)
+    {
+          _context = context;
+    }
+    //Get api/users
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+         return await _context.Users.ToListAsync();
+    }
+    //Get api/usuarios/id 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUsersById(int id)
+    {
+        return await _context.Users.FindAsync(id); 
+    }
+}
