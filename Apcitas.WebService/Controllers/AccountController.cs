7e4892020530d4b30627344fae9f3f77b2a7e142
goracijCerv <100542978+goracijCerv@users.noreply.github.com>
2022-09-12 16:25:07 -0500

    Se hacen los cambios del 12/sep
    Se creo la carpeta de DTOs y la clase de request, ahora ya funciona la primer acción de postman appcitas

--- Apcitas.WebService/Controllers/AccountController.cs
+++ Apcitas.WebService/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using Apcitas.WebService.Data;
+using Apcitas.WebService.DTOs;
 using Apcitas.WebService.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -16,17 +18,26 @@ public class AccountController : BaseApiController
     }
 
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(string username, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
+        if (await UserExists(registerDto.Username))
+            return BadRequest("Username is already taken!");
         using var hmac = new HMACSHA512();
         var user = new AppUser
         {
-            UserName = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
         return user;
     }
+
+    #region Private Methods
+    private async Task<bool> UserExists(string username)
+    {
+        return await _context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
+    }
+    #endregion
 }
---
+++ Apcitas.WebService/DTOs/RegisterDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Apcitas.WebService.DTOs;
+
+public class RegisterDto
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
