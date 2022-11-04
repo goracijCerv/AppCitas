ac9c17aa13622f2859a86c3904bc241956a9426d
goracijCerv <100542978+goracijCerv@users.noreply.github.com>
2022-11-04 10:22:33 -0600

    se añadio que se pueda dar like y ver quien te ha dado like
    cambios del dia 4 de noviembre

---
+++ Apcitas.WebService/Controllers/LikesController.cs
@@ -0,0 +1,55 @@
+﻿using Apcitas.WebService.DTOs;
+using Apcitas.WebService.Entities;
+using Apcitas.WebService.Extensions;
+using Apcitas.WebService.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apcitas.WebService.Controllers;
+
+public class LikesController : BaseApiController
+{
+    private readonly IUserRepository _userRepository;
+    private readonly ILikeRepository _likeRepository;
+
+    public LikesController(ILikeRepository likeRepository, IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+        _likeRepository = likeRepository;
+    }
+
+    [HttpPost("{username}")]
+    public async Task<ActionResult> AddLike(string username)
+    {
+        var sourceUserId = User.GetUserId();
+        var likedUser = await _userRepository.GetUserByUsernameAsync(username);
+        var sourceUser = await _likeRepository.GetUserWithLikes(sourceUserId);
+
+        if (likedUser == null) return NotFound();
+
+        if (sourceUser.UserName.Equals(username)) return BadRequest("you can´t like yourself");
+
+        var userLike = await _likeRepository.GetUserLike(sourceUserId, likedUser.Id);
+
+        if (userLike != null) return BadRequest("you alredy like this user");
+
+        userLike = new UserLike
+        {
+            SourceUserId = sourceUserId,
+            LikedUserId = likedUser.Id
+        };
+
+        sourceUser.LikedUsers.Add(userLike);
+
+        if (await _userRepository.SaveAllAsync()) return Ok();
+
+        return BadRequest("Faild yo like user");
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<LikeDto>>> GetUserLikes(string predicate)
+    {
+        var users = await _likeRepository.GetUserLikes(predicate, User.GetUserId());
+        return Ok(users);
+    }
+
+}
--- Apcitas.WebService/Data/LikesRepository.cs
+++ Apcitas.WebService/Data/LikesRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Apcitas.WebService.DTOs;
 using Apcitas.WebService.Entities;
+using Apcitas.WebService.Extensions;
 using Apcitas.WebService.Interfaces;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 
 namespace Apcitas.WebService.Data;
 
@@ -16,18 +18,44 @@ public class LikesRepository : ILikeRepository
         _mapper = mapper;
     }
 
-    public Task<UserLike> GetUserLike(int sourceUserId, int likedUserId)
+    public async Task<UserLike> GetUserLike(int sourceUserId, int likedUserId)
     {
-        throw new NotImplementedException();
+        return await _context.Likes.FindAsync(sourceUserId, likedUserId);
     }
 
-    public Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+    public async Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
     {
-        throw new NotImplementedException();
+        var users = _context.Users.OrderBy(u => u.UserName).AsQueryable();
+        var likes = _context.Likes.AsQueryable();
+
+        if (predicate.ToLower().Equals("liked"))
+        {
+            likes = likes.Where(like => like.SourceUserId == userId);
+            users = likes.Select(like => like.LikedUser);
+        }
+
+        if (predicate.ToLower().Equals("likedby"))
+        {
+            likes = likes.Where(like => like.LikedUserId == userId);
+            users = likes.Select(like => like.SourceUser);
+        }
+
+        return await users.Select(user => new LikeDto
+        {
+            Username = user.UserName,
+            KnownAs = user.KnowAs,
+            Age = user.DateOfBirth.CalculeteAge(),
+            PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain).Url,
+            City = user.City,
+            Id = user.Id
+        }).ToListAsync();
+
     }
 
-    public Task<AppUser> GetUserWithLikes(int userId)
+    public async Task<AppUser> GetUserWithLikes(int userId)
     {
-        throw new NotImplementedException();
+        return await _context.Users
+            .Include(x => x.LikedUsers)
+            .FirstOrDefaultAsync(x => x.Id == userId);
     }
 }
